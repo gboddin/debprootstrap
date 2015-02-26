0a0cbfaf330368accdef264159788ad36d088620
Gregory Boddin <gregory.boddin@ext.ec.europa.eu>
2015-02-26 18:09:10 +0100

    added customized cdebootstrap config to avoid ubuntu's apt-helper issues

--- lib/install-header.sh
+++ lib/install-header.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-[ -z $1 ]&&echo Please specify an install dir && exit 1
-[ ! -d $1 ]&&mkdir $1
-[ ! -d $1 ]&&echo Directory $1 not available &&exit 1
-[ $(find $1|wc -l) -gt 1 ]&&echo Directory $1 not empty && exit 1
+[ -z $1 ] && echo Please specify an install dir && exit 1
+[ ! -d $1 ] && mkdir $1
+[ ! -d $1 ] && echo Directory $1 not available &&exit 1
+[ $(find $1|wc -l) -gt 1 ] && echo Directory $1 not empty && exit 1
 echo Installing in $1...
 cat << EEOOFF|base64 -d|tar  --strip-component=1 -zxC $1
