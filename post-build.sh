7b0ff369343cf6ccd1a7e5a4bfe8133d0654fadf
gbo <gregory@siwhine.net>
2015-03-03 03:04:10 +0100

    I should REALLY go to sleep now

--- post-build.sh
+++ post-build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
-[ "${DISTRO}" = "debian" ] && sed "s/SUITE/${DISTRO_SUITE}/g" > build/etc/apt/sources.list <<"EOF"
-deb http://http.debian.net/debian SUITE main contrib non-free
-deb http://ftp.debian.org/debian/  SUITE-updates main contrib non-free
-deb http://security.debian.org/  SUITE/updates main contrib non-free
-EOF
-
-[ "${DISTRO}" = "ubuntu" ] && sed "s/SUITE/${DISTRO_SUITEi}/g"   > build/etc/apt/sources.list <<"EOF"
-#------------------------------------------------------------------------------#
-#                            OFFICIAL UBUNTU REPOS                             #
-#------------------------------------------------------------------------------#
-###### Ubuntu Main Repos
-deb http://archive.ubuntu.com/ubuntu/ SUITE main restricted universe multiverse
-###### Ubuntu Update Repos
-deb http://archive.ubuntu.com/ubuntu/ SUITE-security main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ SUITE-updates main restricted universe multiverse
-EOF
+if [ "${DISTRO}" = "debian" ]; then
+	echo "deb http://http.debian.net/debian $DISTRO_SUITE main contrib non-free"
+	echo "deb http://ftp.debian.org/debian/  ${DISTRO_SUITE}-updates main contrib non-free"
+	echo "deb http://security.debian.org/  ${DISTRO_SUITE}/updates main contrib non-free"
+fi
+if [ "${DISTRO}" = "ubuntu" ]; then 
+	echo "deb http://archive.ubuntu.com/ubuntu/ ${DISTRO_SUITE} main restricted universe multiverse"
+	echo "deb http://archive.ubuntu.com/ubuntu/ ${DISTRO_SUITE}-security main restricted universe multiverse"
+	echo "deb http://archive.ubuntu.com/ubuntu/ ${DISTRO_SUITE}-updates main restricted universe multiverse"
+fi
