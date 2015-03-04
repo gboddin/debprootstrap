bd4702b217af8ab61e6739db63295e207e6efc75
gbo <gregory@siwhine.net>
2015-03-04 17:17:42 +0100

    naming result files

--- build-all.sh
+++ build-all.sh
@@ -3,12 +3,7 @@
 
 for arch in amd64 i386; do
     for distro in wheezy jessie sid; do
-        ./debprootstrap ${arch} ${distro} || exit 1
+        ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}.bundle || exit 1
     done
 done
-# ubuntu
-for arch in amd64 i386; do
-    for distro in  trusty utopic vivid precise; do
-        ./debprootstrap ${arch} ${distro} || exit 1
-    done
-done
+# ubuntu later
