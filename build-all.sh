90baff5d26a2b7bee6b545aa31f7e11822c663b5
gbo <gregory@siwhine.net>
2015-03-11 23:29:54 +0100

    Added trusty in travis builds

--- build-all.sh
+++ build-all.sh
@@ -2,8 +2,7 @@
 # travis wont build for arm : TODO debprootstrap a build environment :)
 version=$(cat version)
 for arch in amd64 i386; do
-    for distro in wheezy jessie sid; do
-        ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}.bundle || exit 1
+    for distro in wheezy jessie sid trusty; do
+        ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
     done
 done
-# ubuntu later
