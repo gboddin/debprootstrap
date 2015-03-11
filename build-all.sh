6201f53ccb58d7e7f43e48d9487fd600daec8429
gbo <gregory@siwhine.net>
2015-03-12 00:22:12 +0100

    Push for build -- trusty only

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 # travis wont build for arm : TODO debprootstrap a build environment :)
 version=$(cat version)
 for arch in amd64 i386; do
-    for distro in wheezy jessie sid trusty; do
+    for distro in trusty; do
         ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
     done
 done
