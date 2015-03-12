9d851d5fdd8faeee5eb507fcc73bd1f56e053191
gbo <gregory@siwhine.net>
2015-03-12 02:07:11 +0100

    Time to sleep :)

--- build-all.sh
+++ build-all.sh
@@ -2,20 +2,20 @@
 
 echo "Building for archictures   : ${DEBPROOTSTRAP_TRAVIS_ARCHS}"
 echo "Building for distributions : ${DEBPROOTSTRAP_TRAVIS_DISTROS}"
-
+echo "Debprootstraping build environment ..."
 ./debprootstrap i386 testing
-
-rsync -av . build/debprootstrap --exclude build
-build/debprootstrap apt-get update
-build/debprootstrap apt-get install qemu
-
+echo "Preparing build enviornment ..."
+rm *bundle
+rsync -a . build/debprootstrap --exclude build
+./build/manage admin apt-get update
+./build/manage admin apt-get install qemu
 version=$(cat version)
 
 for arch in ${DEBPROOTSTRAP_TRAVIS_ARCHS}; do
 
     for distro in ${DEBPROOTSTRAP_TRAVIS_DISTROS}; do
 
-        buid/manage user -w /debprootstrap ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
+        ./build/manage user -w /debprootstrap ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
 
     done
 
