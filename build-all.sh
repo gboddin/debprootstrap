8e98a9d640e2063063b774f5c82136b5149badb2
gbo <gregory@siwhine.net>
2015-03-12 02:34:23 +0100

    Building on sid amd64

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@
 echo "Building for archictures   : ${DEBPROOTSTRAP_TRAVIS_ARCHS}"
 echo "Building for distributions : ${DEBPROOTSTRAP_TRAVIS_DISTROS}"
 echo "Debprootstraping build environment ..."
-./debprootstrap i386 testing
+./debprootstrap amd64 sid 
 echo "Preparing build enviornment ..."
 rm *bundle
 rsync -a . build/debprootstrap --exclude build
