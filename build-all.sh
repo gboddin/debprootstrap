ffb3c16c2496675abf44ec957da7c28afdd494d0
gbo <gregory@siwhine.net>
2015-03-12 01:47:58 +0100

    Let be less verbose then ...

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-# travis wont build for arm : TODO debprootstrap a build environment :)
+
+echo "Building for archictures   : ${DEBPROOTSTRAP_TRAVIS_ARCHS}"
+echo "Building for distributions : ${DEBPROOTSTRAP_TRAVIS_DISTROS}"
+
 version=$(cat version)
+
 for arch in ${DEBPROOTSTRAP_TRAVIS_ARCHS}; do
+
     for distro in ${DEBPROOTSTRAP_TRAVIS_DISTROS}; do
+
         ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
+
     done
+
 done
