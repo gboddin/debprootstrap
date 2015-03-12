a3dc76d2a5ab3f150818ff88809dc3eeee6ce8f2
gbo <gregory@siwhine.net>
2015-03-13 00:45:54 +0100

    Build matrix

--- build-all.sh
+++ build-all.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-
-echo "Building for archictures   : ${DEBPROOTSTRAP_TRAVIS_ARCHS}"
-echo "Building for distributions : ${DEBPROOTSTRAP_TRAVIS_DISTROS}"
-echo "Debprootstraping build environment ..."
-./debprootstrap amd64 stable 
-echo "Preparing build enviornment ..."
-rm *bundle
-rsync -a . build/debprootstrap --exclude build
-./build/manage admin apt-get update
-./build/manage admin apt-get install qemu -y
-./build/manage admin apt-get install xz-utils -y
-version=$(cat version)
-
-for arch in ${DEBPROOTSTRAP_TRAVIS_ARCHS}; do
-
-    for distro in ${DEBPROOTSTRAP_TRAVIS_DISTROS}; do
-
-        ./build/manage user -w /debprootstrap ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
-
+for arch in armhf amd64 i386; do
+    for distro in stable testing unstable vivid utopic precise lucid; do
+	echo "    - env: D_ARCH=\"${arch}\" D_SUITE=\"${distro}\""
     done
-
 done
-mv build/debprootstrap/*bundle .
-ls -lha *bundle
