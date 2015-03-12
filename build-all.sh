c75d9a01d499a636951e60b08cac4a104f95fc9a
gbo <gregory@siwhine.net>
2015-03-12 02:01:14 +0100

    Building inside a debproot container (multliarch build ftw, arm is coming)

--- build-all.sh
+++ build-all.sh
@@ -3,14 +3,21 @@
 echo "Building for archictures   : ${DEBPROOTSTRAP_TRAVIS_ARCHS}"
 echo "Building for distributions : ${DEBPROOTSTRAP_TRAVIS_DISTROS}"
 
+./debprootstrap i386 testing
+
+rsync -av . build/debprootstrap --exclude build
+build/debprootstrap apt-get update
+build/debprootstrap apt-get install qemu
+
 version=$(cat version)
 
 for arch in ${DEBPROOTSTRAP_TRAVIS_ARCHS}; do
 
     for distro in ${DEBPROOTSTRAP_TRAVIS_DISTROS}; do
 
-        ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
+        buid/manage user -w /debprootstrap ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
 
     done
 
 done
+mv build/debprootstrap/*bundle .
