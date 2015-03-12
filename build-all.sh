843b9e98432b9f7519c78cbf3b97c9be24a638f8
gbo <gregory@siwhine.net>
2015-03-12 01:18:00 +0100

    Passing build params trought environment for travis

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # travis wont build for arm : TODO debprootstrap a build environment :)
 version=$(cat version)
-for arch in amd64 i386; do
-    for distro in trusty; do
+for arch in ${DEBPROOTSTRAP_TRAVIS_ARCHS}; do
+    for distro in ${DEBPROOTSTRAP_TRAVIS_DISTROS}; do
         ./debprootstrap ${arch} ${distro} install-${distro}-${arch}-${version}-$(date +%Y%m%d).bundle || exit 1
     done
 done
