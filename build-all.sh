606f5e2971dedb4df41da6347e5aa4dbf32719e1
gbo <gregory@siwhine.net>
2015-03-13 01:26:26 +0100

    test should use proot-qemu helper

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for arch in armhf amd64 i386; do
-    for distro in stable testing unstable vivid utopic precise lucid; do
+    for distro in wheezy jessie sid vivid utopic precise ; do
 	echo "    - env: D_ARCH=\"${arch}\" D_SUITE=\"${distro}\""
     done
 done
